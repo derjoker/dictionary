c9295c876289e0d76091c1c37ff5d47372c2694a
Feng Ya <fengya04@126.com>
2018-11-14 19:27:42 +0800

    load dataset

--- src/duden/index.js
+++ src/duden/index.js
@@ -1,15 +1,39 @@
 const { resolve } = require('path')
-const { readdirSync, readFileSync } = require('fs')
+const { readdirSync, readFileSync, writeFileSync } = require('fs')
 const { load } = require('cheerio')
 
 const extract = require('./extract')
 
-const path = resolve(__dirname, 'downloads')
+let DEBUG = false
+// DEBUG = true
+const path = resolve(__dirname, DEBUG ? 'samples' : 'downloads')
 const files = readdirSync(path)
 
+const log = 'duden.log'
+if (!DEBUG) writeFileSync(log, '')
+
+const loader = 'load.js'
+writeFileSync(
+  loader,
+  'db = db.getSiblingDB("dictionary");\n' +
+    'db.duden.drop();\n' +
+    'db.duden.insertMany([\n'
+)
+
 files.forEach(file => {
+  console.log(file)
   const html = readFileSync(resolve(path, file))
   const $ = load(html)
-  const entry = extract($)
-  console.log(JSON.stringify(entry, null, '  '))
+  try {
+    const entry = extract($)
+    // console.log(JSON.stringify(entry, null, '  '))
+    writeFileSync(loader, JSON.stringify(entry, null, '  ') + ',\n', {
+      flag: 'a'
+    })
+  } catch (error) {
+    writeFileSync(log, file + '\n', { flag: 'a' })
+    // throw error
+  }
 })
+
+writeFileSync(loader, ']);', { flag: 'a' })
